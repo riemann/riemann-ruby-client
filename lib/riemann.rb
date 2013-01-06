c031041bbcb3ea9fa4a65fecf5e382f213ae8b08
b <b@b3k.us>
2013-01-06 14:14:07 -0800

    Arbitrary attributes on events

--- lib/riemann.rb
+++ lib/riemann.rb
@@ -5,6 +5,7 @@ module Riemann
   require 'beefcake'
   require 'riemann/version'
   require 'riemann/state'
+  require 'riemann/attribute'
   require 'riemann/event'
   require 'riemann/query'
   require 'riemann/message'
